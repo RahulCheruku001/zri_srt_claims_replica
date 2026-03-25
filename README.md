6186daa21c3cfd0fbabf44a0728141239e29680d
rahul.cheruku@msg-global.com <rahul.cheruku@msg-global.com>
2026-03-25 08:24:27 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,42 @@
+## Application Details
+|               |
+| ------------- |
+|**Generation Date and Time**<br>Wed Mar 25 2026 07:56:02 GMT+0000 (Coordinated Universal Time)|
+|**App Generator**<br>SAP Fiori Application Generator|
+|**App Generator Version**<br>1.21.0|
+|**Generation Platform**<br>SAP Business Application Studio|
+|**Template Used**<br>List Report Page V2|
+|**Service Type**<br>SAP System (ABAP On-Premise)|
+|**Service URL**<br>http://mgss81a.aws.msg.de:44300/sap/opu/odata/sap/ZRI_SB_CLM_PROC|
+|**Module Name**<br>zsrt_claims|
+|**Application Title**<br>Claims Replication|
+|**Namespace**<br>srt.claims|
+|**UI5 Theme**<br>sap_horizon|
+|**UI5 Version**<br>1.120.14|
+|**Enable TypeScript**<br>False|
+|**Add Eslint configuration**<br>False|
+|**Main Entity**<br>Claims|
+
+## zsrt_claims
+
+Claims Replication
+
+### Starting the generated app
+
+-   This app has been generated using the SAP Fiori tools - App Generator, as part of the SAP Fiori tools suite.  To launch the generated application, run the following from the generated application root folder:
+
+```
+    npm start
+```
+
+- It is also possible to run the application using mock data that reflects the OData Service URL supplied during application generation.  In order to run the application with Mock Data, run the following from the generated app root folder:
+
+```
+    npm run start-mock
+```
+
+#### Pre-requisites:
+
+1. Active NodeJS LTS (Long Term Support) version and associated supported NPM version.  (See https://nodejs.org)
+
+
---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,172 @@
+sap.ui.define([
+    "sap/m/MessageToast",
+    "sap/m/Dialog",
+    "sap/m/Table",
+    "sap/m/Column",
+    "sap/m/Text",
+    "sap/m/ColumnListItem",
+    "sap/m/Button",
+    "sap/ui/model/json/JSONModel",
+    "sap/ui/export/Spreadsheet"
+], function (MessageToast, Dialog, Table, Column, Text, ColumnListItem, Button, JSONModel, Spreadsheet) {
+    'use strict';
+
+    return {
+        get_Results: function (oEvent) {
+
+                var oModel = this.getView().getModel();
+                var aContexts = this.extensionAPI.getSelectedContexts();
+                var that = this;
+
+                var aResults = [];
+                var iCompleted = 0;
+
+                this.getView().setBusy(true);
+                for (let index = 0; index < aContexts.length; index++) {
+
+                    var oData = aContexts[index].getObject();
+
+                    var oPayload = {
+                        schadnr: oData.schadnr
+                    };
+
+                    oModel.callFunction("/show_res", {
+                        method: "POST",
+                        urlParameters: oPayload,
+
+                        success: function (oResult) {
+
+                            var oResultData = oResult.results;
+
+                            // store result
+                            aResults.push(oResultData);
+
+                            iCompleted++;
+
+                            // when all calls finished
+                            if (iCompleted === aContexts.length) {
+                                that.getView().setBusy(false);
+                                that._showResultPopup(aResults);
+
+                            }
+
+                        },
+
+                        error: function () {
+                            iCompleted++;
+
+                            if (iCompleted === aContexts.length) {
+                                that._showResultPopup(aResults);
+                            }
+
+                            MessageToast.show("Error calling backend");
+                        }
+
+                    });
+                }
+            },
+
+
+            _showResultPopup: function (aData) {
+
+                // Flatten nested arrays
+                var aFlatData = aData.flat();
+
+                var sthat = this;
+
+                var oModel = new sap.ui.model.json.JSONModel();
+                oModel.setData({ results: aFlatData });
+
+                var oTable = new sap.m.Table({
+                    columns: [
+                        new sap.m.Column({
+                            header: new sap.m.Text({ text: "Source Loss Number" }),
+                            width: "10%" // smaller width
+                        }),
+                        new sap.m.Column({
+                            header: new sap.m.Text({ text: "Target Loss Number" }),
+                            width: "10%"
+                        }),
+                        new sap.m.Column({
+                            header: new sap.m.Text({ text: "Process Ref Id" }),
+                            width: "10%"
+                        }),
+                        new sap.m.Column({
+                            header: new sap.m.Text({ text: "Status" }),
+                            width: "10%"
+                        }),
+                        new sap.m.Column({
+                            header: new sap.m.Text({ text: "Message" }),
+                            width: "60%" // bigger width
+                        })
+                    ]
+                });
+
+                oTable.setModel(oModel);
+
+                oTable.bindItems({
+                    path: "/results",
+                    template: new sap.m.ColumnListItem({
+                        cells: [
+                            new sap.m.Text({ text: "{Source_No}" }),
+                            new sap.m.Text({ text: "{Target_No}" }),
+                            new sap.m.Text({ text: "{Process_Id}" }),
+                            new sap.m.Text({ text: "{Status}" }),
+                            new sap.m.Text({ text: "{Message}" })
+                        ]
+                    })
+                });
+
+                var oDialog = new sap.m.Dialog({
+                    title: "Business Partner Copy Logs",
+                    content: [oTable],
+                    buttons: [
+                        new sap.m.Button({
+                            text: "Export",
+                            type: "Emphasized",
+                            press: function () {
+                                sthat._exportToExcel(aFlatData);
+                            }
+                        }),
+                        new sap.m.Button({
+                            text: "Close",
+                            press: function () {
+                                oDialog.close();
+                            }
+                        })
+                    ],
+                    afterClose: function () {
+                        oDialog.destroy();
+                    }
+                });
+
+                oDialog.open();
+            },
+
+            _exportToExcel: function (aData) {
+
+                var aCols = [
+                    { label: "Source Loss Number", property: "Source_No" },
+                    { label: "Target Loss Number", property: "Target_No" },
+                    { label: "Copy Ref Id", property: "Process_Id" },
+                    { label: "Status", property: "Status" },
+                    { label: "Message", property: "Message" }
+                ];
+
+                var oSettings = {
+                    workbook: {
+                        columns: aCols
+                    },
+                    dataSource: aData,
+                    fileName: "Claims_Copy_Results.xlsx"
+                };
+
+                var oSheet = new sap.ui.export.Spreadsheet(oSettings);
+                oSheet.build().finally(function () {
+                    oSheet.destroy();
+                });
+            }
+
+        };
+
+});
